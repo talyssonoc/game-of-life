a44332481172c3b2c95418249beb3b9601ef8e51
talyssonoc <a11023@bcc.unifal-mg.edu.br>
2013-11-26 21:13:57 -0200

    File correction

--- js/init.js
+++ js/init.js
@@ -1,4 +1,4 @@
 window.addEventListener('load', function() {
-  // window.gameOfLife = new GameOfLife(50, 50, 250);
-  window.gameOfLife = new GameOfLife(90, 200, 50);
+  window.gameOfLife = new GameOfLife(50, 50, 250);
+  // window.gameOfLife = new GameOfLife(90, 200, 50);
 });
